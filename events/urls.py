661b8b1e1514ad0151b012894f673dd528b6c935
John Elder <johne4196@gmail.com>
2021-03-16 09:23:05 -0700

    search venues

--- events/urls.py
+++ events/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
 	path('<int:year>/<str:month>/', views.home, name="home"),
 	path('events', views.all_events, name="list-events"),
 	path('add_venue', views.add_venue, name='add-venue'),
+	path('list_venues', views.list_venues, name='list-venues'),
+	path('show_venue/<venue_id>', views.show_venue, name='show-venue'),	
+	path('search_venues', views.search_venues, name='search-venues'),
+	
+
 ]
--- events/views.py
+++ events/views.py
@@ -3,9 +3,34 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from django.http import HttpResponseRedirect
-from .models import Event
+from .models import Event, Venue
 from .forms import VenueForm
 
+def search_venues(request):
+	if request.method == "POST":
+		searched = request.POST['searched']
+		venues = Venue.objects.filter(name__contains=searched)
+	
+		return render(request, 
+		'events/search_venues.html', 
+		{'searched':searched,
+		'venues':venues})
+	else:
+		return render(request, 
+		'events/search_venues.html', 
+		{})
+
+
+
+def show_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	return render(request, 'events/show_venue.html', 
+		{'venue': venue})
+
+def list_venues(request):
+	venue_list = Venue.objects.all()
+	return render(request, 'events/venue.html', 
+		{'venue_list': venue_list})
 
 def add_venue(request):
 	submitted = False
