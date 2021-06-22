cdae94287ddf7acaea184f88ef1754ceec093ed9
John Elder <johne4196@gmail.com>
2021-06-22 09:51:23 -0700

    tweaked admin titles

--- myclub_website/urls.py
+++ myclub_website/urls.py
@@ -5,3 +5,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
 ]
+
+# Configure Admin Titles
+admin.site.site_header = "My Club Administration Page"
+admin.site.site_title = "Browser Title"
+admin.site.index_title = "Welcome To The Addmin Area..."
