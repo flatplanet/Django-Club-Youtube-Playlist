34e34bbc8e369dfb6f5a6696ed4913b2035ea1e7
John Elder <johne4196@gmail.com>
2021-08-04 10:00:29 -0700

    register users

--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login_user', views.login_user, name="login"),
+    path('logout_user', views.logout_user, name='logout'),
+    path('register_user', views.register_user, name='register_user'),
 ]
--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def login_user(request):
 	if request.method == "POST":
@@ -18,5 +19,27 @@ def login_user(request):
 	else:
 		return render(request, 'authenticate/login.html', {})
 
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You Were Logged Out!"))
+	return redirect('home')
 
+
+def register_user(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Registration Successful!"))
+			return redirect('home')
+	else:
+		form = UserCreationForm()
+
+	return render(request, 'authenticate/register_user.html', {
+		'form':form,
+		})
 
