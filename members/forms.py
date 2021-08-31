591633e8693a4685e6b288718cbc03797ce5a4ec
John Elder <johne4196@gmail.com>
2021-08-31 09:54:01 -0700

    determine correct user

---
+++ members/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterUserForm(UserCreationForm):
+	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+	first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+	last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class':'form-control'}))
+	
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+	def __init__(self, *args, **kwargs):
+		super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs['class'] = 'form-control'
+		self.fields['password1'].widget.attrs['class'] = 'form-control'
+		self.fields['password2'].widget.attrs['class'] = 'form-control'
--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 def login_user(request):
 	if request.method == "POST":
@@ -27,7 +28,7 @@ def logout_user(request):
 
 def register_user(request):
 	if request.method == "POST":
-		form = UserCreationForm(request.POST)
+		form = RegisterUserForm(request.POST)
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data['username']
@@ -37,7 +38,7 @@ def register_user(request):
 			messages.success(request, ("Registration Successful!"))
 			return redirect('home')
 	else:
-		form = UserCreationForm()
+		form = RegisterUserForm()
 
 	return render(request, 'authenticate/register_user.html', {
 		'form':form,
