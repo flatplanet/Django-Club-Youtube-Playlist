042847c1e44dcde122305ad969a9f90147ef1073
John Elder <johne4196@gmail.com>
2021-12-14 11:00:20 -0800

    add venue images

--- myclub_website/settings.py
+++ myclub_website/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
